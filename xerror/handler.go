f90e69296bf5d95a0d8edba7237bfdfe14ac18cf
soluty <soluty.lv@qq.comn>
2022-10-11 11:53:41 +0000

    aa

--- xerror/handler.go
+++ xerror/handler.go
@@ -2,9 +2,15 @@ package xerror
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/soluty/go/xlog"
 )
+
+// HandleError 处理那些不知道该怎么处理的错误，一般是通过日志打印出来，也可以写到其它的地方
+func HandleError(ctx context.Context, err error)  {
+	fmt.Println(111)
+}
 
 func handle(){
 	xlog.Info(context.Background(), "abc")
